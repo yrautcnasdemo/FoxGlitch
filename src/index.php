7a71d4a98477784316cc013f92251b0e4cb2ab52
Guilain De Meyer <yrautcnas@msn.com>
2025-09-17 15:04:23 +0200

    login/inscirption OK

--- src/index.php
+++ src/index.php
@@ -1,4 +1,8 @@
 <?php
+if ($_SERVER["REQUEST_METHOD"] === "POST" && isset($_POST["form_type"])) {
+// Si quelqu’un a soumis un formulaire en POST, et que ce formulaire contient bien un champ form_type, alors on continue =>
+
+
 // Choix du formulaire a traiter (Login ou Inscription)
 if ($_POST["form_type"] === "login") {
 // traitement login
@@ -80,7 +84,7 @@ if ($_POST["form_type"] === "login") {
     }
 
 }
-
+}
 ?>
 
 <!DOCTYPE html>
@@ -134,14 +138,14 @@ if ($_POST["form_type"] === "login") {
                         <path stroke-linecap="round" stroke-linejoin="round" d="M21.75 6.75v10.5a2.25 2.25 0 0 1-2.25 2.25h-15a2.25 2.25 0 0 1-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0 0 19.5 4.5h-15a2.25 2.25 0 0 0-2.25 2.25m19.5 0v.243a2.25 2.25 0 0 1-1.07 1.916l-7.5 4.615a2.25 2.25 0 0 1-2.36 0L3.32 8.91a2.25 2.25 0 0 1-1.07-1.916V6.75" />
                     </svg>
 
-                        <input type="email" class="login-input" placeholder="YourMail@Adress.com" require>
+                        <input type="email" class="login-input" name="email" placeholder="YourMail@Adress.com" require>
                     </div>
                     <div class="input-wrapper">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 5.25a3 3 0 0 1 3 3m3 0a6 6 0 0 1-7.029 5.912c-.563-.097-1.159.026-1.563.43L10.5 17.25H8.25v2.25H6v2.25H2.25v-2.818c0-.597.237-1.17.659-1.591l6.499-6.499c.404-.404.527-1 .43-1.563A6 6 0 1 1 21.75 8.25Z" />
                     </svg>
 
-                        <input type="password" placeholder="Enter password" class="login-input">
+                        <input type="password" name="pass" placeholder="Enter password" class="login-input">
                     </div>
                     <div class="rem-for">
                         <label for="remember-me">
